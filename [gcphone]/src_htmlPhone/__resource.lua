6847183befda658c93b3d1786c675507a70234dc
atsuky <66851649+atsuky@users.noreply.github.com>
2020-07-10 15:34:52 +0200

    add gcphone

---
+++ [gcphone]/src_htmlPhone/__resource.lua
@@ -0,0 +1,40 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+client_scripts {
+	'build/build.js',
+	'check-versions.js',
+	'dev-client.js',
+	'dev-server.js',
+	'utils.js',
+	'vue-loader.conf.js',
+	'webpack.base.conf.js',
+	'webpack.dev.conf.js',
+	'webpack.prod.conf.js',
+	'dev.env.js',
+	'index.js',
+	'prod.env.js',
+	'index.html',
+
+
+}
+
+server_scripts {
+
+	'build/build.js',
+	'check-versions.js',
+	'dev-client.js',
+	'dev-server.js',
+	'utils.js',
+	'vue-loader.conf.js',
+	'webpack.base.conf.js',
+	'webpack.dev.conf.js',
+	'webpack.prod.conf.js',
+	'dev.env.js',
+	'index.js',
+	'prod.env.js',
+	'index.html',
+
+
+
+
+}
